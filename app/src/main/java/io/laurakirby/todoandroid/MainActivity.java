a49f899911414a6ce7c623c0a409205be7462a43
LauraKirby <laurakirby26@gmail.com>
2016-06-29 15:58:11 -0700

    click on a list item to edit content, click 'update' to see the item updated

--- app/src/main/java/io/laurakirby/todoandroid/MainActivity.java
+++ app/src/main/java/io/laurakirby/todoandroid/MainActivity.java
@@ -2,11 +2,14 @@ package io.laurakirby.todoandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import org.apache.commons.io.FileUtils;
 
@@ -23,11 +26,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        lvItems = (ListView)findViewById(R.id.lvItems);
+        lvItems = (ListView) findViewById(R.id.lvItems);
         readItems();
         itemsAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, items);
         lvItems.setAdapter(itemsAdapter);
         setupListViewListener();
+
     }
 
     private void setupListViewListener(){
@@ -42,14 +46,31 @@ public class MainActivity extends AppCompatActivity {
                     }
                 }
         );
+        lvItems.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> adapter, View item, int pos, long id) {
+                        Button updateBtn = (Button)findViewById(R.id.btnAddItem);
+                        EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
+                        String clickedItem = (String) lvItems.getItemAtPosition(pos);
+                        etNewItem.setText(clickedItem);
+                        String itemText = etNewItem.getText().toString();
+                        items.remove(pos);
+                        itemsAdapter.notifyDataSetChanged();
+                        updateBtn.setText("Update");
+                    }
+                }
+        );
     }
 
-    public void onAddItem(View v){
+    public void onAddOrEditItem(View v){
+        Button updateBtn = (Button)findViewById(R.id.btnAddItem);
         EditText etNewItem = (EditText) findViewById(R.id.etNewItem);
         String itemText = etNewItem.getText().toString();
         itemsAdapter.add(itemText);
         etNewItem.setText("");
         writeItems();
+        updateBtn.setText("Add Item");
     }
 
     private void readItems(){
